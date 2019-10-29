e7f8259cd2bf5177a510ca10a1fc34eb5c95824a
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-29 09:13:03 -0500

    mvp for favorite things complete. Wishlist prints to DOM after button is pressed.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 // When the button is clicked, display the data in the following format in the DOM.
 
 // I can purchase {thing} at {location}
+
+const domContainer = document.querySelector("#container")
+const saveButton = document.querySelector("#saveButton")
+
+
+
+saveButton.addEventListener("click", () => {
+    const favThing = document.querySelector("#favoriteThing").value
+    const favStore = document.querySelector("#store").value
+
+    domContainer.innerHTML += `
+    <p>I can purchase ${favThing} at ${favStore}.</p>
+    `
+    
+})
